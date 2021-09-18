08ea2a312581703c366168bbf4dd0fff075d5071
Yash <yashgarg0905@gmail.com>
2021-09-18 23:21:39 +0530

    Updates

---
+++ precache-manifest.8f5a1905954d8fbe578536d841d1e964.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da32bea913a7c4d3ef7757f272953587",
+    "url": "/NetflixClone/index.html"
+  },
+  {
+    "revision": "d562c23b2d3885dd9809",
+    "url": "/NetflixClone/static/css/main.ee589de0.chunk.css"
+  },
+  {
+    "revision": "8db2ed269914238549d6",
+    "url": "/NetflixClone/static/js/2.5f60033b.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/NetflixClone/static/js/2.5f60033b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d562c23b2d3885dd9809",
+    "url": "/NetflixClone/static/js/main.bdf1a618.chunk.js"
+  },
+  {
+    "revision": "23df55bba4ca6686f05a",
+    "url": "/NetflixClone/static/js/runtime-main.6133a437.js"
+  }
+]);
